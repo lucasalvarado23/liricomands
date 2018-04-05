638510ddb270f55879fb1e1391fb00cff81a1166
Lucas <lucasalvarado23@gmail.com>
2018-04-05 17:56:19 -0400

    finished liri

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var keys = require("keys")
+var keys = require("./keys")
 
 var Twitter = require("twitter");
 
@@ -12,10 +12,22 @@ var spotify = new Spotify(keys.spotify);
 
 var client = new Twitter(keys.twitter);
 
-/* `my-tweets`
+client.get('statuses/user_timeline', function(error, tweets, response) {
+    if (error) throw error;
+    let parsedTweets = tweets
+    for (var i = 0; i < parsedTweets.length; i++) {
+        console.log("tweet ", i+1, ": ",parsedTweets[i].text)
+    }
+});
 
-* `spotify-this-song`
 
-* `movie-this`
+spotify.search({
+    type: 'track',
+    query: 'All the Small Things'
+}, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
 
-* `do-what-it-says`
+    console.log(data);
+});
